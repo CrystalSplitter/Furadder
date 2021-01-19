83a6068c666ec481e0b1e60efc3a147d6584550b
CrystalSplitter <gamewhizzit@gmail.com>
2021-01-18 16:38:02 -0800

    Add year extraction utils to shared_utilities.js

--- content_scripts/shared_utilities.js
+++ content_scripts/shared_utilities.js
@@ -1,4 +1,62 @@
-"use strict";
+const MONTH_TO_NUM = {
+  jan: 1,
+  feb: 2,
+  mar: 3,
+  apr: 4,
+  may: 5,
+  jun: 6,
+  jul: 7,
+  aug: 8,
+  sep: 9,
+  oct: 10,
+  nov: 11,
+  dec: 12,
+};
+
+const enUSLangYearFunc = (s) => {
+  const re = /([A-Z][a-z]+)\s+([0-9]?[0-9]),\s+([0-9]+)/;
+  const found = s.match(re);
+  if (found && MONTH_TO_NUM[found[1].toLowerCase()]) {
+    return {
+      day: parseInt(found[2]),
+      month: parseInt(MONTH_TO_NUM[found[1].toLowerCase()]),
+      year: parseInt(found[3]),
+    };
+  }
+  return null;
+};
+
+const enGBLangYearFunc = (s) => {
+  const re = /([0-9]?[0-9]),?\s+([A-Z][a-z]+),?\s+([0-9]+)/;
+  const found = s.match(re);
+  if (found && MONTH_TO_NUM[found[2].toLowerCase()]) {
+    return {
+      day: parseInt(found[1]),
+      month: parseInt(MONTH_TO_NUM[found[2].toLowerCase()]),
+      year: parseInt(found[3]),
+    };
+  }
+  return null;
+};
+
+const isoYearFunc = (s) => {
+  const re = /([0-9]+)-([0-9][0-9])-([0-9]+)/;
+  const found = s.match(re);
+  if (found) {
+    return {
+      day: parseInt(found[3]),
+      month: parseInt(found[2]),
+      year: parseInt(found[1]),
+    };
+  }
+  return null;
+};
+
+const LANGUAGE_YEAR_MAPPINGS = {
+  en: enUSLangYearFunc,
+  "en-US": enUSLangYearFunc,
+  "en-GB": enGBLangYearFunc,
+};
 
 function newImageObject(props) {
   return {
@@ -27,4 +85,14 @@ function sizeCompare(img1, img2) {
     return 1;
   }
   return 0;
+}
+
+/**
+ *
+ */
+function parseDateString(s, lang) {
+  if (LANGUAGE_YEAR_MAPPINGS[lang] === undefined) {
+    return isoYearFunc(s);
+  }
+  return LANGUAGE_YEAR_MAPPINGS[lang](s);
 }
