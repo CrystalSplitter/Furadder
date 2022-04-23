6251cf2ee34cc0959184021ec64b6611ab8f3757
CrystalSplitter <gamewhizzit@gmail.com>
2022-04-23 01:17:04 -0700

    Update version_bump.py for multi-browser

--- version_bump.py
+++ version_bump.py
@@ -3,33 +3,46 @@
 
 import argparse
 import json
+import pathlib
+import typing as t
+
+BROWSER_DIRS = ["chromium", "firefox"]
 
 
 def main():
     """Bump the version."""
     args = parse_args()
-    with open("manifest.json", "r") as f:
-        manifest_dict = json.loads(f.read())
+    for manifest_path in _get_manifests(pathlib.Path.cwd()):
+        with open(manifest_path, "r") as f:
+            manifest_dict = json.loads(f.read())
+        _bump_manifest(args.version_type, manifest_dict)
+        with open(manifest_path, "w") as f:
+            f.write(json.dumps(manifest_dict, indent=2, sort_keys=True))
+
+
+def _bump_manifest(version_type: str, manifest_dict: t.Dict[str, t.Any]):
     version_string = manifest_dict["version"]
     split_version = version_string.split(".")
     major = int(split_version[0])
     middle = int(split_version[1])
     minor = int(split_version[2])
-    if (args.version_type == "major"):
+    if version_type == "major":
         major += 1
         middle = 0
         minor = 0
-    elif (args.version_type == "middle"):
+    elif version_type == "middle":
         middle += 1
         minor = 0
-    elif (args.version_type == "minor"):
+    elif version_type == "minor":
         minor += 1
     else:
-        raise ValueError(f"Unknown version type: {args.version_type}")
+        raise ValueError(f"Unknown version type: {version_type}")
     manifest_dict["version"] = ".".join(
         [str(x) for x in [major, middle, minor]])
-    with open("manifest.json", "w") as f:
-        f.write(json.dumps(manifest_dict, indent=4, sort_keys=True))
+
+
+def _get_manifests(root: pathlib.Path):
+    return [root.joinpath(d + "/manifest.json") for d in BROWSER_DIRS]
 
 
 def parse_args():
