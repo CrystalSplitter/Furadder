0a06424cd5667a05e7d5f56dfc16d8c301606585
CrystalSplitter <gamewhizzit@gmail.com>
2022-04-23 23:48:33 -0700

    Remove matching_sites.js
    This was unused and had no purpose.

--- shared/popup/js_src/matching_sites.js
+++
@@ -1,7 +0,0 @@
-import twitterHandler from "twitter_handler";
-
-export const MATCHING_TABLE = [
-  [/twitter/, { name: "Twitter", handler: twitterHandler }],
-  [/derpibooru/, { name: "Derpibooru", handler: null }],
-  [/deviantart/, { name: "DeviantArt", handler: null }],
-];
