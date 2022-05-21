02068fdd76da9820fa242f116e179dbac394dc46
CrystalSplitter <gamewhizzit@gmail.com>
2022-05-20 22:17:48 -0700

    Add furaffinity year extraction

--- shared/content_scripts/furaffinity_handler.ts
+++ shared/content_scripts/furaffinity_handler.ts
@@ -45,6 +45,30 @@
     return elem.textContent.trim();
   }
 
+  function getYear(): number | null {
+    const datetime = document
+      .querySelector("span.popup_date")
+      ?.getAttribute("title");
+    if (datetime == null) {
+      consoleError("Could not get <span> tag with popup_date class");
+      return null;
+    }
+    const year = parseDateString(datetime, "en-US");
+    if (year == null) {
+      consoleError(`datetime ${datetime} was not en-US formatted`);
+      return null;
+    }
+    return year.year;
+  }
+
+  function getTags(): string[] {
+    const year = getYear();
+    if (year != null) {
+      return [year.toString()];
+    }
+    return [];
+  }
+
   function listener(request: ExtractorRequest): Promise<Feedback> {
     const { command, data } = request;
     if (command === "contentExtractData") {
@@ -59,6 +83,7 @@
             expectedResolutions: getExpectedRes(),
             authors: getArtists(),
             description: getDescription(),
+            extractedTags: getTags(),
           }).resolvePromise();
         case "general":
           consoleDebug("Using general server fetch");
--- shared/content_scripts/shared_utilities.ts
+++ shared/content_scripts/shared_utilities.ts
@@ -102,6 +102,7 @@ const LANGUAGE_YEAR_MAPPINGS = {
   en: enUSLangYearFunc,
   "en-US": enUSLangYearFunc,
   "en-GB": enGBLangYearFunc,
+  iso8601: isoYearFunc,
 };
 
 function newImageObject(props: Partial<ImageObj>): ImageObj {
@@ -139,7 +140,7 @@ function sizeCompare(img1: HTMLImageElement, img2: HTMLImageElement): number {
 /**
  * Browser language types.
  */
-type Lang = "en" | "en-US" | "en-GB";
+type Lang = "en" | "en-US" | "en-GB" | "iso8601";
 
 /**
  * Parse a date string using a region format.
