910c92bb66d6ed953d4331b757887afde5ccf6f9
CrystalSplitter <gamewhizzit@gmail.com>
2022-06-23 21:59:05 -0700

    Add better DeviantArt resolution extraction
    This uses the intermediary hack, which doesn't always work,
    but works pretty okay for older images (pre 2018).

--- shared/content_scripts/shared_utilities.ts
+++ shared/content_scripts/shared_utilities.ts
@@ -191,3 +191,24 @@ function escapeMarkdown(s: string): string {
     .replaceAll("`", "\\`")
     .replaceAll("]", "\\]");
 }
+
+/**
+ * Fetch with a ms timeout.
+ * @param resource Fetch RequestInfo to pass through.
+ * @param options Fetch options, with an added :timeout: number entry (in ms)
+ * @returns
+ */
+async function fetchWithTimeout(
+  resource: RequestInfo,
+  options: any = {}
+): Promise<Response> {
+  const { timeout = 4000 } = options;
+  const controller = new AbortController();
+  const id = setTimeout(() => controller.abort(), timeout);
+  const response = await fetch(resource, {
+    ...options,
+    signal: controller.signal,
+  });
+  clearTimeout(id);
+  return response;
+}
